ef5894aca6d7e81f7fbfa0003df41bc83181db84
Anand <codeaim2@gmail.com>
2026-06-28 19:09:57 +0530

    feat: Add Google Tag Manager integration to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,11 +79,30 @@ export default async function RootLayout({
   return (
     <html lang="en" className="h-full scroll-smooth" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MBQCBC45');`}
+        </Script>
+        {/* End Google Tag Manager */}
         {gscVerification ? (
           <meta name="google-site-verification" content={gscVerification} />
         ) : null}
       </head>
       <body className={`min-h-full flex flex-col font-display bg-white text-secondary ${inter.variable}`} suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MBQCBC45"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {gaId ? (
           <>
             <Script
